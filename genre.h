c949c2418502753adda96cc11bee2324a65c8635
kevin maussion <KevinMaussion@users.noreply.github.com>
2014-12-08 09:34:57 +0100

    Create genre.h

---
+++ genre.h
@@ -0,0 +1,18 @@
+#ifndef GENRE_H_INCLUDED
+#define GENRE_H_INCLUDED
+
+typedef enum Genre{
+    AUCUN,
+    ACTION,
+    HORREUR,
+    COMEDIE,
+    DOCUMENTAIRE,
+    POLICIER,
+    DRAME,
+    ANIMATION,
+    SCIENCE_FICTION
+}Genre;
+
+
+
+#endif // GENRE_H_INCLUDED
